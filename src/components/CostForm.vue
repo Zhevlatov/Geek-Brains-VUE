b0287dc78b7360ce79512ebef0ce59334bc3aa8f
Zhevlatov <alex.zhevlatov@mail.ru>
2022-10-28 00:46:02 +0300

    dz4

---
+++ src/components/CostForm.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="form">
+        <input class="item" v-model="category" type="text" placeholder="Payment description" />
+        <input class="item" v-model="value" type="text" placeholder="Payment amount" />
+        <input class="item" v-model="date" type="text" placeholder="Payment date" />
+        <button v-on:click="add">ADD</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TaskForm',
+    data() {
+        return {
+            category: '',
+            value: '',
+            date: ''
+        }
+    },
+    methods: {
+        add() {
+            this.$emit('add', this.date, this.category, this.value)
+            this.category = ''
+            this.value = ''
+            this.date = ''
+        }
+    }
+}
+</script>
+<style lang="scss">
+.form {
+    margin: 0 auto;
+    margin-top: 15px;
+    display: flex;
+    flex-direction: column;
+    width: 200px;
+
+    .item {
+        height: 30px;
+        margin: 3px;
+        border: 1px solid lightgrey;
+        box-shadow: 0 0 10px 2px lightgrey;
+    }
+}
+</style>
---
+++ src/components/CostList.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="list">
+        <div :class="{ item: true, header: item.number == '#' }" v-for="item of data" v-bind:key="item.id">
+            <span class="number">{{ item.number }}</span>
+            <span class="data">{{ item.date }}</span>
+            <span class="data">{{ item.category }}</span>
+            <span class="data">{{ item.value }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CostList',
+    props: {
+        data: {
+            type: Array,
+            default: []
+        },
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style lang="scss">
+.list {
+    width: 500px;
+    margin: 0 auto;
+
+    .header {
+        font-weight: 700;
+    }
+
+    .item {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        border: 1px solid lightgray;
+
+        .number {
+            width: 40px;
+        }
+
+        .data {
+            width: 150px;
+        }
+    }
+}
+</style>
